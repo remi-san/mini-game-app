65e9e77e9d97a57ff2ab662d7241958f9466194f
Rémi San <remi.san@gmail.com>
2015-07-04 13:03:12 +0200

    added database manager impl

---
+++ src/Manager/InDatabaseGameManager.php
@@ -0,0 +1,117 @@
+<?php
+namespace MiniGameApp\Manager;
+
+use Doctrine\ORM\ORMException;
+use MiniGame\GameOptions;
+use MiniGame\MiniGame;
+use MiniGame\Player;
+use MiniGame\Repository\MiniGameRepository;
+use MiniGameApp\Manager\Exceptions\GameNotFoundException;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+abstract class InDatabaseGameManager implements GameManager, LoggerAwareInterface
+{
+    /**
+     * @var MiniGameRepository
+     */
+    protected $repository;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * Constructor
+     *
+     * @param MiniGameRepository $repository
+     */
+    public function __construct(MiniGameRepository $repository)
+    {
+        $this->logger = new NullLogger();
+    }
+
+    /**
+     * Create a mini-game according to the options
+     *
+     * @param  GameOptions $options
+     * @return MiniGame
+     */
+    abstract public function createMiniGame(GameOptions $options);
+
+    /**
+     * Saves a mini-game
+     *
+     * @param  MiniGame $game
+     * @return MiniGame
+     */
+    public function saveMiniGame(MiniGame $game)
+    {
+        $this->repository->save($game);
+    }
+
+
+    /**
+     * Get the mini-game corresponding to the id
+     *
+     * @param  string $id
+     * @return MiniGame
+     * @throws GameNotFoundException
+     */
+    public function getMiniGame($id)
+    {
+        $game = null;
+        try {
+            $game = $this->repository->find($id);
+        } catch (ORMException $e) {}
+
+        if (!$game) {
+            throw new GameNotFoundException('Game with id "' . $id . '" doesn\'t exist!');
+        }
+        return $game;
+    }
+
+    /**
+     * Get the active mini-game for the player
+     *
+     * @param Player $player
+     * @return MiniGame
+     * @throws GameNotFoundException
+     */
+    public function getActiveMiniGameForPlayer(Player $player)
+    {
+        $game = null;
+        try {
+            $game = $this->repository->findPlayerMinigame($player);
+        } catch (ORMException $e) {}
+
+        if (!$game) {
+            throw new GameNotFoundException('Game with for user "' . $player->getId() . '" doesn\'t exist!');
+        }
+        return $game;
+    }
+
+    /**
+     * Delete the mini-game corresponding to the id
+     *
+     * @param  string $id
+     * @return void
+     * @throws GameNotFoundException
+     */
+    public function deleteMiniGame($id)
+    {
+        $game = $this->getMiniGame($id);
+        $this->repository->delete($game);
+    }
+
+    /**
+     * @param  LoggerInterface $logger
+     * @return void
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+} 
---
+++ src/Manager/InDatabasePlayerManager.php
@@ -0,0 +1,110 @@
+<?php
+namespace MiniGameApp\Manager;
+
+use MiniGame\Player;
+use MiniGame\Repository\PlayerRepository;
+use MiniGameApp\Manager\Exceptions\PlayerException;
+use MiniGameApp\Manager\Exceptions\PlayerNotFoundException;
+use MiniGameApp\Manager\Exceptions\UnbuildablePlayerException;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+abstract class InDatabasePlayerManager implements PlayerManager, LoggerAwareInterface
+{
+    /**
+     * @var PlayerRepository
+     */
+    protected $repository;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * Constructor
+     *
+     * @param PlayerRepository $repository
+     */
+    public function __construct(PlayerRepository $repository)
+    {
+        $this->repository = $repository;
+        $this->logger = new NullLogger();
+    }
+
+    /**
+     * Retrieves a player
+     *
+     * @param  int $id
+     * @return Player
+     * @throws PlayerNotFoundException
+     */
+    public function get($id)
+    {
+        $player = $this->repository->find($id);
+        if (!$player) {
+            throw new PlayerNotFoundException('Player with id "' . $id . '" doesn\'t exist!');
+        }
+        return $player;
+    }
+
+    /**
+     * Gets the user id from the user object
+     *
+     * @param  object $object
+     * @throws PlayerException
+     * @return string
+     */
+    protected abstract function getUserId($object);
+
+    /**
+     * Retrieves a player
+     *
+     * @param  object $object
+     * @throws PlayerException
+     * @return Player
+     */
+    public function getByObject($object)
+    {
+        $userId = $this->getUserId($object);
+        return $this->get($userId);
+    }
+
+    /**
+     * Creates a player
+     *
+     * @param  object $object
+     * @throws UnbuildablePlayerException
+     * @return Player
+     */
+    public abstract function create($object);
+
+    /**
+     * Saves a player
+     *
+     * @param  Player $player
+     * @return void
+     */
+    public function save(Player $player)
+    {
+        $this->repository->save($player);
+    }
+
+    /**
+     * Can the player manager deal with that object?
+     *
+     * @param  object $object
+     * @return boolean
+     */
+    protected abstract function supports($object);
+
+    /**
+     * @param  LoggerInterface $logger
+     * @return void
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+} 
