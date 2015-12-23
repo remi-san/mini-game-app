dd377b0ac275a49e0c70a81e3493dae99dcb949e
Rémi San <remi.san@gmail.com>
2015-12-23 21:10:49 +0100

    added typing

--- src/ReadModel/Player.php
+++ src/ReadModel/Player.php
@@ -1,9 +1,17 @@
 <?php
 namespace MiniGameApp\ReadModel;
 
+use MiniGame\Entity\PlayerId;
+
 interface Player
 {
+    /**
+     * @return PlayerId
+     */
     public function getId();
 
+    /**
+     * @return MiniGame
+     */
     public function getGame();
 }
