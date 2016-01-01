03d98f57398ca9ad2400db52943eb21a97a9e866
Rémi San <remi.san@gmail.com>
2016-01-01 22:01:04 +0100

    used logger aware trait

--- src/Handler/MiniGameCommandHandler.php
+++ src/Handler/MiniGameCommandHandler.php
@@ -9,12 +9,15 @@ use MiniGameApp\Event\MiniGameAppErrorEvent;
 use MiniGameApp\MiniGameFactory;
 use MiniGameApp\Repository\GameRepository;
 use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 use RemiSan\Command\ErrorEventHandler;
 
 class MiniGameCommandHandler implements LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     /**
      * @var MiniGameFactory
      */
@@ -29,11 +32,6 @@ class MiniGameCommandHandler implements LoggerAwareInterface
      * @var ErrorEventHandler
      */
     private $errorHandler;
-
-    /**
-     * @var LoggerInterface
-     */
-    protected $logger;
 
     /**
      * Constructor
@@ -124,17 +122,5 @@ class MiniGameCommandHandler implements LoggerAwareInterface
                 )
             );
         }
-    }
-
-    /**
-     * Sets a logger instance on the object
-     *
-     * @param  LoggerInterface $logger
-     * @return void
-     * @codeCoverageIgnore
-     */
-    public function setLogger(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
     }
 }
